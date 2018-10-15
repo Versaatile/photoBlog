c49bf7480e2d4c1eea2121713a43fb15ae008b53
Camboni Alex <alex.camboni@intotheweb.be>
2018-10-15 22:36:17 +0200

    Text animation ok , line verify overflow

--- src/app/component/loader/loader.component.ts
+++ src/app/component/loader/loader.component.ts
@@ -21,18 +21,17 @@ import {
 
       state('closed', style({
         opacity: 1,
-        transform: 'rotate(30deg)',
-        transformOrigin: 'bottom right',
-        overflow: 'hidden',
-        color: 'black'
+        transform: 'rotate(-30deg) translateY(-50px)',
+        transformOrigin: 'bottom left',
+        color: 'white',
       })),
 
       transition('closed => open', [
-        animate('0.7s ease-in')
+        animate('0.5s ease-in')
       ]),
 
       transition('open => closed', [
-        animate('0.7s ease-in')
+        animate('0.5s ease-in')
       ]),
     ]),
 
@@ -44,18 +43,33 @@ import {
 
       state('open', style({
         opacity: 1,
-        transform: 'rotateZ(30deg)',
+        transform: 'rotate(30deg) translateY(100px)',
         transformOrigin: 'bottom left',
-        width: 0,
         color: 'white',
       })),
 
       transition('closed => open', [
-        animate('0.7s ease-in')
+        animate('0.5s 1.5s ease-in'),
       ]),
 
       transition('open => closed', [
-        animate('0.7s ease-in')
+        animate('0.5s ease-in')
+      ]),
+    ]),
+
+
+    trigger('firstLine', [
+      state('open', style({
+        opacity: 1,
+      })),
+
+      state('closed', style({
+        opacity: 1,
+        width: 0,
+      })),
+
+      transition('closed => open', [
+        animate('0.5s ease-out'),
       ]),
     ]),
 
@@ -78,21 +92,19 @@ import {
     trigger('backline', [
       state('open', style({
         opacity: 1,
-        left: 200,
-        width: 0
+        transform: 'translateX(1000px)',
       })),
 
       state('closed', style({
         opacity: 1,
-        left: 0,
       })),
 
       transition('closed => open', [
-        animate('0.5s ease-in'),
+        animate('0.5s 1.8s ease-in'),
       ]),
 
       transition('open => closed', [
-        animate('0.5s 0.5s ease-in'),
+        animate('0.5s ease-in'),
       ]),
     ])]
 })
@@ -100,11 +112,14 @@ import {
 export class LoaderComponent implements OnInit {
   public triggerText = false;
   public triggerTextBack = false;
+  public triggerFirstLine = false;
   public triggerLine = false;
   public triggerBackline = true;
   public triggerLineBack = false;
   public displayfirstLine = true;
   public displayfirstText = true;
+
+  public text: String = 'Fenrir\'s photo';
 
 
 
@@ -117,6 +132,12 @@ export class LoaderComponent implements OnInit {
   }
 
   public textAnimationDone(event) {
+    if (event.fromState === 'closed') {
+      this.triggerFirstLine = !this.triggerFirstLine;
+    }
+  }
+
+  public firstLineAnimationDone(event) {
     if (event.fromState === 'closed') {
       this.triggerLine = !this.triggerLine;
       this.displayfirstText = !this.displayfirstText;
