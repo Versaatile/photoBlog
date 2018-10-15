a0aad9f4df5d07534a1ba2517ffa0db5273f3137
Camboni Alex <alex.camboni@intotheweb.be>
2018-10-15 07:09:04 +0200

    to be continued

--- src/app/component/loader/loader.component.ts
+++ src/app/component/loader/loader.component.ts
@@ -16,23 +16,46 @@ import {
     trigger('text', [
       state('open', style({
         opacity: 1,
-        transformOrigin: 'bo left',
         color: 'white',
       })),
 
       state('closed', style({
         opacity: 1,
-        transform: 'rotate(19deg)',
+        transform: 'rotate(30deg)',
         transformOrigin: 'bottom right',
+        overflow: 'hidden',
         color: 'black'
       })),
 
       transition('closed => open', [
-        animate('0.5s ease-in')
+        animate('0.7s ease-in')
       ]),
 
       transition('open => closed', [
-        animate('0.5s ease-in')
+        animate('0.7s ease-in')
+      ]),
+    ]),
+
+    trigger('textBack', [
+      state('closed', style({
+        opacity: 1,
+        color: 'white',
+      })),
+
+      state('open', style({
+        opacity: 1,
+        transform: 'rotateZ(30deg)',
+        transformOrigin: 'bottom left',
+        width: 0,
+        color: 'white',
+      })),
+
+      transition('closed => open', [
+        animate('0.7s ease-in')
+      ]),
+
+      transition('open => closed', [
+        animate('0.7s ease-in')
       ]),
     ]),
 
@@ -76,23 +99,27 @@ import {
 
 export class LoaderComponent implements OnInit {
   public triggerText = false;
+  public triggerTextBack = false;
   public triggerLine = false;
-  public triggerBackline = false;
+  public triggerBackline = true;
   public triggerLineBack = false;
   public displayfirstLine = true;
+  public displayfirstText = true;
+
 
 
   constructor() { }
 
   ngOnInit() {
     if (this.triggerText === false) {
-      setTimeout(() => this.triggerText = true);
+      setTimeout(() => this.triggerText = !this.triggerText);
     }
   }
 
   public textAnimationDone(event) {
     if (event.fromState === 'closed') {
-      this.triggerLine = true;
+      this.triggerLine = !this.triggerLine;
+      this.displayfirstText = !this.displayfirstText;
     }
   }
 
@@ -101,6 +128,8 @@ export class LoaderComponent implements OnInit {
     if (event.fromState === 'closed') {
       this.displayfirstLine = !this.displayfirstLine;
       this.triggerText = !this.triggerText;
+      this.triggerTextBack = !this.triggerTextBack;
+
     }
   }
 
