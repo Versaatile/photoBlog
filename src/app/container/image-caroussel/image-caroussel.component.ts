94c580f345460b1cf61c32737ccce929c4195071
Camboni Alex <alex.camboni@intotheweb.be>
2018-11-15 14:47:48 +0100

    hey

--- src/app/container/image-caroussel/image-caroussel.component.ts
+++ src/app/container/image-caroussel/image-caroussel.component.ts
@@ -9,18 +9,25 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 })
 export class ImageCarousselComponent implements OnInit {
 
+  public showNavigationArrows = false;
+  public showNavigationIndicators = true;
+  public oldActive = false;
   public images = [];
   constructor() {
-    this.images.push('../../../assets/IMG_3018-2.jpg');
     this.images.push('../../../assets/IMG_4884.jpg');
-    this.images.push('../../../assets/IMG_5123.jpg');
     this.images.push('../../../assets/IMG_6717.jpg');
-    this.images.push('../../../assets/IMG_9124.jpg');
   }
 
   ngOnInit() {
   }
 
+  carouselMouseDown(event) {
+    this.oldActive = true;
+  }
+
+  carouselMouseUp(event) {
+    this.oldActive = false;
+  }
 }
 
 
